0bfe6f7e4746d652cca955bd94be0bf664de8dc1
williamkimball <36681575+williamkimball@users.noreply.github.com>
2018-08-27 11:01:07 -0500

    finished exercise

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Bug
+{
+    /*
+        You can declare a typed public property, make it read-only,
+        and initialize it with a default value all on the same
+        line of code in C#. Readonly properties can be set in the
+        class' constructors, but not by external code.
+    */
+    public string Name { get; } = "";
+    public string Species { get; } = "";
+    public ICollection<string> Predators { get; } = new List<string>();
+    public ICollection<string> Prey { get; } = new List<string>();
+
+    public string FormalName => $"{this.Name} the {this.Species}";
+
+    // Class constructor
+    public Bug(string name, string species, List<string> predators, List<string> prey)
+    {
+        this.Name = name;
+        this.Species = species;
+        this.Predators = predators;
+        this.Prey = prey;
+    }
+
+    // Convert this method to an expression member
+    public string PreyList() => string.Join(",", this.Prey);
+
+    // Convert this method to an expression member
+
+    public string PredatorList() => string.Join(",", this.Predators);
+
+    // Convert this to expression method (hint: use a C# ternary)
+        public string Eat(string food) => this.Prey.Contains(food) ? $"{this.Name} ate the {food}." : $"{this.Name} is still hungry.";};
+        
+
+namespace expression_members
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
